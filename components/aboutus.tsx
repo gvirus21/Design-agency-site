a078fb61b53261ab60f16a2efc949ef7567fe68e
gvirus21 <gvirus21yt@gmail.com>
2022-09-24 12:12:04 +0530

    - Added App logo and scroll icon - Implemented About us section - implemented parallax scroll animation

---
+++ components/aboutus.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Member from "./member";
+import Team from '../data/Team.json'
+
+const Aboutus: React.FC = () => {
+  return (
+    <section className={`flex flex-col bg-white py-20 text-3xl md:text-4xl`}>
+      <div className="container mx-auto px-11">
+        <p className="leading-tight max-w-5xl mx-auto text-4xl lg:text-4xl tracking-tight">
+          <strong>We will Help you ship better apps, faster.</strong> Our team
+          of expert engineers has created the best user experiences in some of
+          the most popular apps worldwide.
+        </p>
+      </div>
+      <div className="container mx-auto px-11 text-center mt-28">
+        <h2>Our Team</h2>
+        <div className="mt-2">The "spec-ops"</div>
+        <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 lg:gap-20">
+
+          {
+            Team.map((member) => {
+                return(
+                    <Member id={member.id} name={member.name} socialId={member.socialId} link={member.link}/>
+                )
+            })
+          } 
+      </div>
+      </div>
+    </section>
+  );
+};
+
+export default Aboutus;
--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,25 +1,66 @@
-import React, {FC} from 'react'
-import Image from 'next/image'
+import React, { FC, useRef, useContext, useState, useCallback } from "react";
+import Image from "next/image";
+import { ScrollContext } from "../utils/scroll-observer";
 
+const Masthead: FC = () => {
+    const refContainer = useRef<HTMLDivElement>(null)
+    const {scrollY} = useContext(ScrollContext)
 
-const Masthead: FC = () =>  {
+    let progress = 0
+    const { current: elContainer } = refContainer
 
-    return(
-        <div className='min-h-screen flex flex-col items-center justify-center'>
-            <video autoPlay loop muted playsInline className='absolute w-full h-full object-cover'>
-                <source src="/assets/bg-2.mp4" type="video/mp4" />
-            </video>
+    if (elContainer ) {
+        progress = Math.min(1, scrollY / elContainer.clientHeight)
+    }
 
-            <div className='p-12 font-bold text-white 
-            drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col'>
-                <h1 className='mb-6 text-4xl xl:text-5xl'>App Storm</h1>
-                <h2 className='mb-2 text-2xl xl:text-3xl tracking-tight'>
-                    <span>Design, </span><span>done right.</span>
-                </h2>
-            </div>
-        </div>
-    )
+  return (
+    <div 
+        ref={refContainer}
+        className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
 
-}
+        style={{
+            transform: `translateY(-${progress * 20}vh)`
+        }}
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute w-full h-full object-cover"
+      >
+        <source src="/assets/bg-2.mp4" type="video/mp4" />
+      </video>
 
-export default Masthead
+      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+        <Image
+          src="/assets/logo.svg"
+          width={128 / 3}
+          height={114 / 3}
+          alt="logo"
+        />
+      </div>
+
+      <div
+        className="p-12 font-bold text-white 
+            drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col"
+      >
+        <h1 className="mb-6 text-4xl xl:text-5xl">Likuid</h1>
+        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
+          <span>Design </span>
+          <span>done right.</span>
+        </h2>
+      </div>
+      <div className="flex-grow-0 pb-20 md:pb-10 transition-all duration-1000">
+        <Image
+          src="/assets/arrow-down.png"
+          width={188 / 3}
+          height={105 / 3}
+          alt="Scroll Down"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Masthead;
