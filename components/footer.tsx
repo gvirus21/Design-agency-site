ba5a9bd68dec67fcab9c06cbbbd355d42d15ec9d
gvirus21 <gouravkumar21.dev@gmail.com>
2022-12-10 12:03:23 +0530

    optimized footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center h-[50vh] w-screen bg-black pl-32 pt-20">
+    <div className="flex flex-col lg:flex-row justify-between items-center h-[60vh] lg:h-[50vh] w-screen bg-black px-10 lg:pl-32 pt-40 lg:pt-20">
       <div className="-mt-24">
-        <h1 className="text-white text-5xl max-w-[35rem]">Like our work? We build excellent apps and website.</h1>
-        <h1 className="text-white text-4xl mt-20">Drop us a message <span className="underline cursor-pointer">here</span></h1>
+        <h1 className="text-white text-4xl md:text-5xl md:max-w-[35rem]">Like our work? We build excellent apps and website.</h1>
+        <h1 className="text-white text-3xl md:text-4xl mt-20">Drop us a message <span className="underline cursor-pointer">here</span></h1>
       </div>
       <div className="py-10">
-        <ul className="flex flex-col justify-between h-32 w-[20rem] text-white text-2xl">
+        <ul className="flex  lg:flex-col justify-between  lg:h-32 w-[20rem] text-white text-2xl">
           <li className="cursor-pointer">LinkedIn</li>
           <li className="cursor-pointer">Twitter</li>
           <li className="cursor-pointer">Instagram</li>
