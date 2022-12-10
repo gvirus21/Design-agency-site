c54d46dc0a7277ba6f113950714ceee08202ae7d
gvirus21 <gouravkumar21.dev@gmail.com>
2022-12-10 14:01:47 +0530

    Optimized responsiveness of the trust page heading

--- components/trustedby.tsx
+++ components/trustedby.tsx
@@ -5,7 +5,7 @@ const TrustedBy = () => {
   return (
     <div className="bg-white min-h-[80vh] flex flex-col justify-center gap-16 md:gap-32">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl mb-20">Trusted by the apps you use.</h1>
+        <h1 className="text-3xl text-center font-semibold md:text-5xl mb-20">Trusted by the apps you use.</h1>
         <ClientLogos />
       </div>
     </div>
